d63865ae7da03220a771455c925fb60eb7f5888d
Shannon Sorensen <sorensen.sms@gmail.com>
2021-09-24 10:07:27 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  if(rideDistance <= 400){
+    return 'This one is on me!';
+} else if(rideDistance > 2000 && rideDistance < 2500){
+  return 'I will gladly take your thirty bucks.';
+} else if(rideDistance >= 2500){
+  return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(checkCity){
+  if (checkCity === 'NYC'){ 
+    return 'Ok, sounds good.';
+  } else {
+    return `No go.`;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(gotTip){
+  if(gotTip === 'generous'){
+    return 'Thank you so much.';
+  } else if(gotTip === 'not as generous'){
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
